78b6598fc20e02002a6ade4864dafc792b0a7f06
AmanPateI <96079253+AmanPateI@users.noreply.github.com>
2022-11-21 15:30:14 -0500

    Update process.h

--- src/process.h
+++ src/process.h
@@ -1,10 +1,3 @@
-/*
- * process.h
- * Multithreaded OS Simulation for CS 2200 and ECE 3058
- *
- * This file contains process data for the simulator.
- */
-
 #ifndef __PROCESS_H__
 #define __PROCESS_H__
 
